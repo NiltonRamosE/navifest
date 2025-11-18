6be8445825e2eb2f938e523631b4219c6c60d9d7
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-11-17 20:09:46 -0500

    chore: imagenes para la galería

--- src/consts/galleryImages.ts
+++ src/consts/galleryImages.ts
@@ -4,18 +4,6 @@ import GalleryImg3 from "@/assets/images/gallery/gallery_03.webp"
 import GalleryImg4 from "@/assets/images/gallery/gallery_04.webp"
 import GalleryImg5 from "@/assets/images/gallery/gallery_05.webp"
 import GalleryImg6 from "@/assets/images/gallery/gallery_06.webp"
-import GalleryImg7 from "@/assets/images/gallery/gallery_07.webp"
-import GalleryImg8 from "@/assets/images/gallery/gallery_08.webp"
-import GalleryImg9 from "@/assets/images/gallery/gallery_09.webp"
-import GalleryImg10 from "@/assets/images/gallery/gallery_10.webp"
-import GalleryImg11 from "@/assets/images/gallery/gallery_11.webp"
-import GalleryImg12 from "@/assets/images/gallery/gallery_12.webp"
-import GalleryImg13 from "@/assets/images/gallery/gallery_13.webp"
-import GalleryImg14 from "@/assets/images/gallery/gallery_14.webp"
-import GalleryImg15 from "@/assets/images/gallery/gallery_15.webp"
-import GalleryImg16 from "@/assets/images/gallery/gallery_16.webp"
-import GalleryImg17 from "@/assets/images/gallery/gallery_17.webp"
-import GalleryImg18 from "@/assets/images/gallery/gallery_18.webp"
 
 export const galleryImages = [
   {
@@ -47,65 +35,5 @@ export const galleryImages = [
     image: GalleryImg6,
     alt: "Galería Navifest 6",
     thumb: "thumb6",
-  },
-  {
-    image: GalleryImg7,
-    alt: "Galería Navifest 7",
-    thumb: "thumb7",
-  },
-  {
-    image: GalleryImg8,
-    alt: "Galería Navifest 8",
-    thumb: "thumb8",
-  },
-  {
-    image: GalleryImg9,
-    alt: "Galería Navifest 9",
-    thumb: "thumb9",
-  },
-  {
-    image: GalleryImg10,
-    alt: "Galería Navifest 10",
-    thumb: "thumb10",
-  },
-  {
-    image: GalleryImg11,
-    alt: "Galería Navifest 11",
-    thumb: "thumb11",
-  },
-  {
-    image: GalleryImg12,
-    alt: "Galería Navifest 12",
-    thumb: "thumb12",
-  },
-  {
-    image: GalleryImg13,
-    alt: "Galería Navifest 13",
-    thumb: "thumb13",
-  },
-  {
-    image: GalleryImg14,
-    alt: "Galería Navifest 14",
-    thumb: "thumb14",
-  },
-  {
-    image: GalleryImg15,
-    alt: "Galería Navifest 15",
-    thumb: "thumb15",
-  },
-  {
-    image: GalleryImg16,
-    alt: "Galería Navifest 16",
-    thumb: "thumb16",
-  },
-  {
-    image: GalleryImg17,
-    alt: "Galería Navifest 17",
-    thumb: "thumb17",
-  },
-  {
-    image: GalleryImg18,
-    alt: "Galería Navifest 18",
-    thumb: "thumb18",
   },
 ]
